30b47a60703b248df7ff224c312860c3434e9036
David Sternheim <DavidSternheim@davids-macbook-pro.local>
2017-01-24 21:58:17 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by David Sternheim on 1/24/17.
+//  Copyright © 2017 David Sternheim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
